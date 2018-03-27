58e323442054c99336f0402bcd96826f91be026d
qiuzhihui <woshiqiuguo@vip.qq.com>
2018-03-27 11:05:17 +0800

    update pratice

--- example/src/test/java/com/qiuhui/test/Mybatis3TestCase.java
+++ example/src/test/java/com/qiuhui/test/Mybatis3TestCase.java
@@ -4,6 +4,7 @@ import com.qiuhui.entity.Movie;
 import com.qiuhui.entity.MovieExample;
 import com.qiuhui.mapper.MovieMapper;
 import com.qiuhui.util.SqlSessionUtil;
+import com.sun.org.glassfish.gmbal.Description;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.After;
 import org.junit.Before;
@@ -18,6 +19,11 @@ public class Mybatis3TestCase {
 
 
 
+    /**
+     *@Description:初始化创建sqlSession和mapper
+     *@Author: 邱志辉
+     *@Date 2018/3/27 0027上午 8:54
+     */
     @Before
     public void init(){
 
@@ -25,11 +31,16 @@ public class Mybatis3TestCase {
         movieMapper = sqlSession.getMapper(MovieMapper.class);
     }
 
+    /**
+     *@Description:根据导演模糊匹配电影List
+     *@Author:邱志辉
+     *@Date 2018/3/27 0027上午 8:53
+     */
     @Test
     public void findListByDirector(){
         MovieExample movieExample  = new MovieExample();
 
-        movieExample.createCriteria().andDirectorLike("%黑泽明%");
+        movieExample.createCriteria().andDirectorLike("%黑泽明%").andIdGreaterThan(122);
 
         List<Movie> movieList = movieMapper.selectByExample(movieExample);
         for(Movie movie : movieList){
@@ -38,7 +49,28 @@ public class Mybatis3TestCase {
 
     }
 
+    @Test
+    public void findListOr(){
+        MovieExample movieExample = new MovieExample();
+        movieExample.or().andRateGreaterThanOrEqualTo(8F);
+        movieExample.or().andReleaseYearBetween("2010","2018");
+        movieExample.setDistinct(true);
+        movieExample.setOrderByClause("release_year desc");
+        List<Movie> movieList = movieMapper.selectByExample(movieExample);
 
+        for(Movie movie : movieList){
+            System.out.println(movie);
+        }
+        }
+
+
+
+
+   /**
+    *@Description:根据id找电影
+    *@Author: 邱志辉
+    *@Date 2018/3/27 0027上午 8:52
+    */
     @Test
     public void findById(){
         Movie movie = movieMapper.selectByPrimaryKey(101);
@@ -48,6 +80,11 @@ public class Mybatis3TestCase {
 
 
 
+    /**
+     *@Description:插入一个电影
+     *@Author: 邱志辉
+     *@Date 2018/3/27 0027上午 8:55
+     */
     @Test
     public void insert(){
 
